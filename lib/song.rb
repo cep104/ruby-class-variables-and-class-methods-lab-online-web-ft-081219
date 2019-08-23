429dc9bb7d20becd6c8bc24643830fbea7288bfd
Candice Peters <candicelizabethpeters@gmail.com>
2019-08-23 21:28:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,12 @@
 class Song 
   @@count = 0
+  @@artists = 0
   attr_accessor :name, :artist, :genre
   def initialize(name,artist,genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
+    @@artists += 1
   end
 end
